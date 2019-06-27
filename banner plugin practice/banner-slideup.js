d9c4edef4215469f836c858750882db0094e2a89
salmon918 <salmon918@doubleservice.com>
2019-06-27 08:50:16 +0800

    completed v1

--- banner plugin practice/banner-slideup.js
+++ banner plugin practice/banner-slideup.js
@@ -23,7 +23,7 @@
             opened: 'opened', // [string]
             opening: 'opening' // [string]
         },
-        transition: true,
+        transition: false,
         whenTransition: function() {
             console.log('whenTransition');
         }
@@ -35,80 +35,140 @@
     var ClassClosing = Module.DEFAULTS.class.closing;
     var ClassOpen = Module.DEFAULTS.class.opened;
     var ClassOpening = Module.DEFAULTS.class.opening;
-    var TransitonOpen = function(){ 
+
+    var TransitonOpen = function($ele, options){
+        $ele
+            .children('.' + options.button.class)
+            .removeClass(ClassOpen)
+            .addClass(ClassClosed)
+            .text(CloseText);
+
         setTimeout(function(){
-            $(this.$ele).removeClass(ClassClosed).addClass(ClassOpening);
-            setTimeout(() => {
-                $(this.$ele).addClass(ClassOpen).removeClass(ClassOpening);
-            }, 500);
-            Module.DEFAULTS.whenTransition();
+            console.log('transition opening');
+
+            $ele.removeClass(ClassClosed).addClass(ClassOpening);
+            setTimeout(function() {
+                console.log('transition opened');
+
+                $ele.addClass(ClassOpen).removeClass(ClassOpening);
+            }, 200);
+            options.whenTransition();
         });
     }
-    var TransitonClose = function(){ 
+    var TransitonClose = function($ele, options){
+
+        $ele
+            .children('.' + options.button.class)
+            .removeClass(ClassOpen)
+            .addClass(ClassClosed)
+            .text(OpenText);
+
         setTimeout(function(){
-            $(this.$ele).removeClass(ClassOpen).addClass(ClassClosing);
-            setTimeout(() => {
-                $(this.$ele).addClass(ClassClosed).removeClass(ClassClosing);
-            }, 500);
-            Module.DEFAULTS.whenTransition();
+            $ele.removeClass(ClassOpen).addClass(ClassClosing);
+            setTimeout(function() {
+                $ele.addClass(ClassClosed).removeClass(ClassClosing);
+            }, 200);
+            options.whenTransition();
         });
     }
 
     Module.prototype.init = function () {
-        $(this.$ele).append('<button class="'+Module.DEFAULTS.button.class+'"></button>');
+        console.log(this.option);
+
+        var that = this;
+        $(this.$ele).append('<button class="'+this.option.button.class+'"></button>');
+
+        this.close();
+
+        if (this.option.autoToggle === true) {
+            that.toggle();
+        }
+
+        if (this.option.autoToggle !== isNaN(this.option.autoToggle)) {
+            setTimeout(function() {
+                that.toggle();
+        }, this.option.autoToggle);
+        }
+
+        if (this.option.openAtStart) {
+            this.open();
+        }
+
+        var ThisBtn = $(this.$ele).children('.' + this.option.button.class);
+        ThisBtn.on('click', function() {
+            that.toggle()
+        });
     }
 
 	Module.prototype.open = function () {
-        console.log($(this.$ele));
-        $(this.$ele).children('button').removeClass(ClassClosed).addClass(ClassOpen).text(CloseText);
-        if (Module.DEFAULTS.transition == true) {
-            TransitonOpen();
+        if (this.option.transition == true) {
+            TransitonOpen(this.$ele, this.option);
         } else {
-            $(this.$ele).removeClass(ClassClosed).addClass(ClassOpen);
+            $(this.$ele)
+                .children('.' + this.option.button.class)
+                .removeClass(ClassClosed)
+                .addClass(ClassOpen)
+                .text(CloseText);
+
+            $(this.$ele)
+                .removeClass(ClassClosed)
+                .addClass(ClassOpen);
         }
 	};
 
 	Module.prototype.close = function () {
-		$(this.$ele).children('button').removeClass(ClassOpen).addClass(ClassClosed).text(OpenText);
-        if (Module.DEFAULTS.transition == true) { 
-            TransitonClose();
+        if (this.option.transition == true) {
+            TransitonClose(this.$ele, this.option);
         } else {
-            $(this.$ele).removeClass(ClassOpen).addClass(ClassClosed);
+            $(this.$ele)
+                .children('.' + this.option.button.class)
+                .removeClass(ClassOpen)
+                .addClass(ClassClosed)
+                .text(OpenText);
+
+            $(this.$ele)
+                .removeClass(ClassOpen)
+                .addClass(ClassClosed);
         }
     };
     
     Module.prototype.toggle = function () {
         var closed = $(this.$ele).hasClass(ClassClosed);
             if (closed) {
-                Module.open();
+                this.open();
             } else {
-                Module.Close();
+                this.close();
             }
     };
-        
+
     $.fn[ModuleName] = function ( methods, options ) {
-		return this.each(function(){
+        console.log(methods, options);
+
+		return this.each(function() {
 			var $this = $(this);
 			var module = $this.data( ModuleName );
+
 			var opts = null;
 			if ( !!module ) {
 				if ( typeof options === 'string') {
                     module.open();
-				} else if ( typeof options === 'string' &&  typeof options2 === 'object' ) {
-					module[options](options2);
+				} else if ( typeof methods === 'string') {
+                    module[methods](options);
 				} else {
 					// console.log('unsupported options!');
 					// throw 'unsupported options!';
 				}
 			} else {
-                opts = $.extend( {}, Module.DEFAULTS, 
-                    ( typeof methods === 'object' && options ), 
+                opts = $.extend( {}, Module.DEFAULTS,
+                    ( typeof methods === 'object' && methods ),
                     ( typeof options === 'object' && options ) );
+
 				module = new Module(this, opts);
+
                 $this.data( ModuleName, module );
+
                 module.init();
-                console.log(module.open());
-			} 
+			}
 		});
 	};
 
@@ -119,7 +179,7 @@ $(function () {
         // 設定一開始是否為開或合
         openAtStart: true, // [boolean] true | false
         // 設定啟動後是否要自動開或合，若設為false，就不要自勳開合；若為true是馬上自動開合；若為數字是幾毫秒之後開合
-        autoToggle: true, // [boolean|number] true | false | 3000
+        autoToggle: false, // [boolean|number] true | false | 3000
         // 設定收合展開按鈕
         button: {
             closeText: '收合', // [string]
@@ -141,11 +201,11 @@ $(function () {
         }
     });
 
-    $('.banner').banner('toggle');
+    // $('.banner').banner('toggle');
+    //
+    // $('.banner').banner('open');
+    //
+    // $('.banner').banner('close');
 
-    $('.banner').banner('open');
-
-    $('.banner').banner('close');
-
-    $('.banner').banner();
+    // $('.banner').banner();
 });
