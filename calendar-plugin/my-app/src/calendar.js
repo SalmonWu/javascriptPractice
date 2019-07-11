3a8af102708802b67c229642afe983649607bef2
Salmon918 <salmon918@gmail.com>
2019-07-11 17:55:55 +0800

    Add calendar table style

--- calendar-plugin/my-app/src/calendar.js
+++ calendar-plugin/my-app/src/calendar.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import TripData from "./data/data1.json";
-// import Moment from "Moment";
+// import moment from "moment";
 
 class Calendar extends Component {
-    constructor(props) {
-        super(props)
+    // constructor(props) {
+    //     super(props)
 
-    }
+    // }
 
     state = {
         'tripData': TripData
@@ -16,14 +16,98 @@ class Calendar extends Component {
     render() {
         return (
             <div className="calendar">
-                <div className="tabs-nav">
+                <div className="cly-header">
                     <ul>
-                        <li className="tabs-link"></li>
-                        <li className="tabs-link"></li>
-                        <li className="tabs-link"></li>
+                        <li><a href={'http://localhost:3000/'}>行程1</a></li>
+                    </ul>
+                    <button type="button" className="ic-ln">切換列表顯示</button>
+                </div>
+                <div className="nv">
+                    <button type="button" className="bt bt-prev"></button>
+                    <button type="button" className="bt bt-next"></button>
+                    <ul className="nvb months">
+                        <li className="nvt">
+                            <a href={'http://localhost:3000/'} className="nvt-link">2019 10月</a>
+                        </li>
+                        <li className="nvt">
+                            <a href={'http://localhost:3000/'} className="nvt-link active">2019 11月</a>
+                        </li>
+                        <li className="nvt">
+                            <a href={'http://localhost:3000/'} className="nvt-link">2019 12月</a>
+                        </li>
                     </ul>
                 </div>
-                <div className="table"></div>
+                <div className="cy-table">
+                    <div className="cy-head weeks">
+                        <ul>
+                            <li>星期日</li>
+                            <li>星期一</li>
+                            <li>星期二</li>
+                            <li>星期三</li>
+                            <li>星期四</li>
+                            <li>星期五</li>
+                            <li>星期六</li>
+                        </ul>
+                    </div>
+                    <div className="cy-body">
+                        <ul>
+                            <li className="disabled"></li>
+                            <li className="has-data">
+                                <ol>
+                                    <li className="days">1</li>
+                                    <li className="guaranteed success"></li>
+                                    <li className="status sign-up">報名</li>
+                                    <li className="vacancy">80</li>
+                                    <li className="total">60</li>
+                                    <li className="price">69888</li>
+                                </ol>
+                            </li>
+                            <li>
+                                <span className="days">2</span>
+                            </li>
+                            <li className="has-data">
+                                <ol>
+                                    <li className="days">3</li>
+                                    <li className="guaranteed"></li>
+                                    <li className="status sign-up">報名</li>
+                                    <li className="vacancy">80</li>
+                                    <li className="total">60</li>
+                                    <li className="price price-up">69888</li>
+                                </ol>
+                            </li>
+                            <li>
+                                <ol>
+                                    <li className="days">4</li>
+
+                                </ol>
+                            </li>
+                            <li>
+                                <ol>
+                                    <li className="days">5</li>
+
+                                </ol>
+                            </li>
+                            <li>
+                                <ol>
+                                    <li className="days">6</li>
+
+                                </ol>
+                            </li>
+                            <li>
+                                <ol>
+                                    <li className="days">7</li>
+
+                                </ol>
+                            </li>
+                            <li>
+                                <ol>
+                                    <li className="days">8</li>
+
+                                </ol>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
