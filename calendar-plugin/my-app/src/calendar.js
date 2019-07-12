4825dba564e3f7a75dcbfad173b6a61f0a176ea3
Salmon918 <salmon918@gmail.com>
2019-07-12 17:59:19 +0800

    add moment

--- calendar-plugin/my-app/src/calendar.js
+++ calendar-plugin/my-app/src/calendar.js
@@ -1,26 +1,137 @@
 import React, { Component } from "react";
 import TripData from "./data/data1.json";
-// import moment from "moment";
+import moment from "moment";
 
-class Calendar extends Component {
-    // constructor(props) {
-    //     super(props)
+class Week extends React.Component {
+    render() {
+        let days = [];
+        let {
+            date,
+        } = this.props;
 
-    // }
+        const {
+            month,
+            selected,
+            select,
+        } = this.props;
 
-    state = {
-        'tripData': TripData
+        for (var i = 0; i < 7; i++) {
+            let day = {
+                name: date.format("dd").substring(0, 1),
+                number: date.date(),
+                isCurrentMonth: date.month() === month.month(),
+                isToday: date.isSame(new Date(), "day"),
+                date: date
+            };
+            days.push(
+                <Day day={day}
+                    selected={selected}
+                    select={select} />
+            );
+
+            date = date.clone();
+            date.add(1, "day");
+        }
+
+        return (
+            <div className="row week" key={days[0]}>
+                {days}
+            </div>
+        );
     }
 
+}
+
+class Day extends React.Component {
+    render() {
+        const {
+            day,
+            day: {
+                date,
+                isCurrentMonth,
+                isToday,
+                number
+            },
+            select,
+            selected
+        } = this.props;
+
+        return (
+            <span
+                key={date.toString()}
+                className={"day" + (isToday ? " today" : "") + (isCurrentMonth ? "" : " different-month") + (date.isSame(selected) ? " selected" : "")}
+                onClick={() => select(day)}>{number}</span>
+        );
+    }
+}
+
+class Calendar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.calendarDisplay = this.calendarDisplay.bind(this)
+    }
+
+    state = {
+        'tripData': TripData,
+        month: moment(),
+        calendarDisplay: true
+    }
+
+    renderWeeks() {
+        let weeks = [];
+        let done = false;
+        let date = this.state.month.clone().startOf("month").add("w" - 1).day("Sunday");
+        let count = 0;
+        let monthIndex = date.month();
+
+        const {
+            selected,
+            month,
+        } = this.state;
+
+        while (!done) {
+            weeks.push(
+                <Week key={date}
+                    date={date.clone()}
+                    month={month}
+                    select={(day) => this.select(day)}
+                    selected={selected} />
+            );
+
+            date.add(1, "w");
+
+            done = count++ > 2 && monthIndex !== date.month();
+            monthIndex = date.month();
+        }
+
+        return weeks;
+    };
+
+
+    calendarDisplay() {
+        if (this.state.calendarDisplay) {
+            this.setState({
+                calendarDisplay: false
+            })
+        } else {
+            this.setState({
+                calendarDisplay: true
+            })
+        }
+    }
 
     render() {
         return (
-            <div className="calendar">
+            <div className={`calendar${this.state.calendarDisplay ? '' : ' display-list'}`}>
                 <div className="cly-header">
                     <ul>
                         <li><a href={'http://localhost:3000/'}>行程1</a></li>
                     </ul>
-                    <button type="button" className="ic-ln">切換列表顯示</button>
+                    <button type="button" className="ic-ln" onClick={this.calendarDisplay}>
+                        <i className="fa fa-list-ul" aria-hidden="true"></i>
+                        切換列表顯示
+                    </button>
                 </div>
                 <div className="nv">
                     <button type="button" className="bt bt-prev"></button>
@@ -50,15 +161,23 @@ class Calendar extends Component {
                         </ul>
                     </div>
                     <div className="cy-body">
+                        {this.renderWeeks()}
+                        {/* <div className="row week" key={days[0]}>
+                            {days}
+                        </div> */}
                         <ul>
-                            <li className="disabled"></li>
+
+                            {/* <li className="disabled"></li>
                             <li className="has-data">
                                 <ol>
-                                    <li className="days">1</li>
+                                    <li className="days">
+                                        1
+                                        <span className="weekday">星期二</span>
+                                    </li>
                                     <li className="guaranteed success"></li>
                                     <li className="status sign-up">報名</li>
-                                    <li className="vacancy">80</li>
-                                    <li className="total">60</li>
+                                    <li className="vacancy">可賣:80</li>
+                                    <li className="total">團位:60</li>
                                     <li className="price">69888</li>
                                 </ol>
                             </li>
@@ -67,11 +186,14 @@ class Calendar extends Component {
                             </li>
                             <li className="has-data">
                                 <ol>
-                                    <li className="days">3</li>
+                                    <li className="days">
+                                        3
+                                        <span className="weekday">星期四</span>
+                                    </li>
                                     <li className="guaranteed"></li>
                                     <li className="status sign-up">報名</li>
-                                    <li className="vacancy">80</li>
-                                    <li className="total">60</li>
+                                    <li className="vacancy">可賣:80</li>
+                                    <li className="total">團位:60</li>
                                     <li className="price price-up">69888</li>
                                 </ol>
                             </li>
@@ -81,30 +203,34 @@ class Calendar extends Component {
 
                                 </ol>
                             </li>
-                            <li>
+                            <li className="has-data holiday">
                                 <ol>
-                                    <li className="days">5</li>
-
+                                    <li className="days">
+                                        5
+                                        <span className="weekday">星期六</span>
+                                    </li>
+                                    <li className="guaranteed"></li>
+                                    <li className="status sign-up">報名</li>
+                                    <li className="vacancy">可賣:80</li>
+                                    <li className="total">團位:60</li>
+                                    <li className="price price-up">69888</li>
                                 </ol>
                             </li>
                             <li>
                                 <ol>
                                     <li className="days">6</li>
-
                                 </ol>
                             </li>
                             <li>
                                 <ol>
                                     <li className="days">7</li>
-
                                 </ol>
                             </li>
                             <li>
                                 <ol>
                                     <li className="days">8</li>
-
                                 </ol>
-                            </li>
+                            </li> */}
                         </ul>
                     </div>
                 </div>
--- calendar-plugin/my-app/src/index.js
+++ calendar-plugin/my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './stylesheets/style.sass';
+import "font-awesome/css/font-awesome.css";
 import Calendar from "./calendar";
 
 ReactDOM.render(<Calendar />, document.getElementById("root"));
